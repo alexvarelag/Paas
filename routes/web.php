4b9a27a021ae498e745e13fa43ca547582c5c7d0
alexvarelag <alexvg0165@gmail.com>
2022-12-03 21:28:35 -0600

    Creacion de Form y Login

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,3 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\LogoutController;
-use App\Http\Controllers\RegisterController;
-
-Route::get('/',[LoginController::class, 'index']);
-Route::post('/login',[LoginController::class, 'validateUser']);
-
-Route::get('/logout',[LogoutController::class, 'index']);
-
-Route::get('/register',[RegisterController::class, 'index']);
-Route::post('/register',[RegisterController::class, 'store']);
