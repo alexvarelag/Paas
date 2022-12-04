42272ef282ff8ec7b261e04c07585ec5ff806a6a
alexvarelag <alexvg0165@gmail.com>
2022-12-03 23:15:53 -0600

    Migracion y Cotroladores

--- resources/views/components/head.blade.php
+++
@@ -1,9 +0,0 @@
-<head>
-    <nav class="navbar navbar-dark bg-dark">
-        <div class="navbar-brand" href="#">
-            <link href="css/style.css" rel="stylesheet"> 
-            <img src="https://cdn-icons-png.flaticon.com/512/4260/4260116.png" width="30" height="30" class="d-inline-block align-top" alt="">
-            Examen
-        </div>
-    </nav>
-</head>
