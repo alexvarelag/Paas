39cf0f60187c305642eff2f43df56d750b0497c7
alexvarelag <alexvg0165@gmail.com>
2022-12-03 23:16:56 -0600

    Leading

---
+++ resources/views/leading.blade.php
@@ -0,0 +1,16 @@
+<x-bootstrap></x-bootstrap>
+
+<head>
+    <title>Login</title>
+    <x-headPrincipal></x-headheadPrincipal>
+</head>
+
+<body>
+    <h1>Examen Final ISW811 - Alex Varela González</h1>
+</body>
+
+<footer>
+    <x-footer></x-footer>
+</footer>
+
+<x-scriptBootstrap></x-scriptBootstrap>
